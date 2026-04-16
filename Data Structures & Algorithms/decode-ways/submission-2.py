19dda2f6f082891d0a6ae5c66505bf58b1f85f3a
ltx-kt <88513803+ltx-kt@users.noreply.github.com>
2026-04-16 17:35:56 -0500

    Add: decode-ways - submission-2

---
+++ Data Structures & Algorithms/decode-ways/submission-2.py
@@ -0,0 +1,19 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        dp1 = 1
+        dp2 = 0
+        curr = 0
+        for i in range(len(s)-1, -1, -1):
+            if s[i] == '0':
+                curr = 0
+            else:
+                curr = dp1
+            
+            if (i < len(s) -1 and 
+            (s[i] == '1' or (s[i] == '2' and s[i+1] in '0123456'))):
+                curr += dp2
+            dp2 = dp1
+            dp1 = curr
+            curr = 0
+        return dp1
+        
