4c6a3d7bddf114a5417a27bd0fade3975b243e6c
ltx-kt <88513803+ltx-kt@users.noreply.github.com>
2026-04-25 00:46:03 -0500

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        maxProfit = 0
+        minBuy = prices[0]
+        for i in prices:
+            maxProfit = max(maxProfit, i - minBuy)
+            minBuy = min(minBuy,i)
+        return maxProfit
