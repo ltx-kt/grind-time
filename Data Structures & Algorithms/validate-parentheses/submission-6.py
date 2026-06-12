325fc3c3287a988688c6567102731516e693f3a8
ltx-kt <88513803+ltx-kt@users.noreply.github.com>
2026-06-11 20:17:10 -0500

    Add: validate-parentheses - submission-6

---
+++ Data Structures & Algorithms/validate-parentheses/submission-6.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        st = []
+        for i in s:
+            print(st)
+            if i in '[{(':
+                st.append(i)
+            if i in '}])':
+                if not any(st):
+                    return False
+                if i == '}' and st.pop() != '{':
+                    return False
+                if i == ')' and st.pop() != '(':
+                    return False
+                if i == ']' and st.pop() != '[':
+                    return False
+        return not st
