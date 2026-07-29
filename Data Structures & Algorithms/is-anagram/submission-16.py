bb88c9acd058ffea33d58ffae29a70317c98c72f
ltx-kt <88513803+ltx-kt@users.noreply.github.com>
2026-07-29 17:08:05 -0500

    Add: is-anagram - submission-16

---
+++ Data Structures & Algorithms/is-anagram/submission-16.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        sd = {}
+        td = {}
+
+        for i in range(len(t)):
+            sd[s[i]] = sd.get(s[i], 0) + 1
+            td[t[i]] = td.get(t[i], 0) + 1
+        return td == sd
