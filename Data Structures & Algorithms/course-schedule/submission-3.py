29ee6a40a3a05716e9108d1b8e6ba45453c9d010
ltx-kt <88513803+ltx-kt@users.noreply.github.com>
2026-08-26 22:48:01 -0500

    Add: course-schedule - submission-3

---
+++ Data Structures & Algorithms/course-schedule/submission-3.py
@@ -0,0 +1,31 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        courseMap = defaultdict(list)
+        for i, j in prerequisites:
+            courseMap[i].append(j)
+        
+        UNVISTED = 0
+        VISTING = 1
+        VISTED = 2
+
+        visitList = [UNVISTED for i in  range(numCourses)]
+
+        def dfs(num):
+            if visitList[num] == VISTED:
+                return True
+            if visitList[num] == VISTING:
+                return False
+
+            visitList[num] = VISTING
+            for nei in courseMap[num]:
+                if not dfs(nei):
+                    return False
+
+            visitList[num] = VISTED
+            return True
+
+
+        for i in range(numCourses):
+            if not dfs(i):
+                return False
+        return True
